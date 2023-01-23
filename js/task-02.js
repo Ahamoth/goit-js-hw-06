c8878fe3f845b5f19c31159dd59108bed9812348
Ahamoth <109838774+Ahamoth@users.noreply.github.com>
2023-01-24 01:41:27 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingridientsList = document.querySelector('ul');
+console.log(ingridientsList);
+
+const elements = ingredients.map((named) => {
+  const itemList = document.createElement('li');
+  itemList.textContent = named;
+  itemList.classList.add('item');
+
+  return itemList;
+  });
+
+  ingridientsList.append(...elements);
