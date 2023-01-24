6d91ed566d1b6573af2044d8a29ceaa7032a3b0e
Ahamoth <109838774+Ahamoth@users.noreply.github.com>
2023-01-24 02:00:28 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+console.log(getRandomHexColor());
+
+const bodyList = document.querySelector('body');
+const spanList = document.querySelector('.color');
+const btnList = document.querySelector('.change-color');
+
+btnList.addEventListener('click', onChangeColorBg);
+
+function onChangeColorBg() {
+  const colorNum = getRandomHexColor();
+  bodyList.style.backgroundColor = colorNum;
+  spanList.textContent = colorNum;
+}
